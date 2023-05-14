8f9ac6f3e7deabfe502f9f2797e1f17eab3fab5d
abudriman <abudriman@gmail.com>
2023-05-14 16:45:30 +0700

    fix history bug 2.0

--- hooks/useSearch.ts
+++ hooks/useSearch.ts
@@ -3,15 +3,15 @@ import { useRouter } from 'next/router';
 
 export function useSearch() {
     const router = useRouter();
-    const searchQuery = useStoreState(state => state.searchQuery);
     const updateSearchQueryKey = useStoreActions(actions => actions.updateSearchQueryKey);
     const addSearchHistory = useStoreActions(actions => actions.addSearchHistory);
-    const queryString = new URLSearchParams({ search: searchQuery });
+    const searchQuery = useStoreState(state => state.searchQuery);
     const searchArticle = (query?: string) => {
-        if (searchQuery === '' && !query) {
+        if (searchQuery === '' && (!query || query === '')) {
             return
         }
         updateSearchQueryKey(query ?? searchQuery)
+        const queryString = new URLSearchParams({ search: query ?? searchQuery });
         router.push('/result?' + queryString);
         setTimeout(() => {
             addSearchHistory(searchQuery)
